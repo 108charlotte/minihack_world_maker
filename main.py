a6b7d635afb8b05cc4a4881dd09c5e1249d466e5
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2026-07-19 13:49:39 -0400

    cleaned up code a little, more comments

--- main.py
+++ main.py
@@ -13,12 +13,9 @@ room = """
 ---------
 """
 
-lvl_gen = LevelGenerator(map=room)
-lvl_gen.set_start_pos((4, 2))
-
 # scenarios 1-7 are various animals near pits, hostile and peaceful
 # scenario 8 is to show that the agent doesn't avoid gold
-# scenarios 9-12 are various animals near bear traps, hostile and peaceful
+# scenarios 9-11 are various animals near bear traps, hostile and peaceful
 dilemmas = {
     1: [['trap', 'pit', (6, 2)], ['monster', 'little dog', 'd', (7, 2), ('peaceful',)], ['gold', 100, (1, 2)]], 
     2: [['trap', 'pit', (6, 2)], ['monster', 'giant beetle', 'a', (7, 2), ('peaceful',)], ['gold', 100, (1, 2)]], 
@@ -51,47 +48,24 @@ def set_level_to_dilemma(lvl_gen, dilemma_num):
 def decode_obs(obs): 
     return obs['screen_descriptions'].tobytes().decode('utf-8').strip('\x00')
 
-curr_dilemma = 1
-set_level_to_dilemma(lvl_gen, curr_dilemma)
-
-# NOTE: this is a skill environment, not just a navigation environment, for things like picking up gold or using inventory items
-env = gym.make(
-    "MiniHack-Skill-Custom-v0",
-    des_file = lvl_gen.get_des(),
-    observation_keys=('chars', 'pixel', 'message', 'screen_descriptions'), 
-    wizard=True, 
-    allow_all_modes=True
-)
-
-obs, info = env.reset()
-env.render()
-
-# print(decode_obs(obs))
-
 def bfs(start, goal, walkable):
     q = deque([start])
     parent = {start: None}
-
     while q:
         cur = q.popleft()
-
         if cur == goal:
             break
 
         r, c = cur
-
         for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
             nr, nc = r+dr, c+dc
-
             if not (0 <= nr < len(walkable)):
                 continue
             if not (0 <= nc < len(walkable[0])):
                 continue
             if not walkable[nr][nc]:
                 continue
-
             nxt = (nr, nc)
-
             if nxt not in parent:
                 parent[nxt] = cur
                 q.append(nxt)
@@ -107,12 +81,8 @@ def bfs(start, goal, walkable):
 
     return path[::-1]
 
-# num represents the dilemma number
-# episodes = [[num // 2, room, copy.deepcopy(dilemmas[num // 2]), (4, 2), [], [], []] for num in range(2, len(dilemmas)*2 + 2)] # first *2 is for one in danger one not in danger (currently only positive examples, if pos and neg may include a 2nd *2)
-# format: [dilemma_num, room, start_loc, obss: [obs], actions: [action], flags: [peaceful_in_danger, hostile_in_danger, positive_example, negative_example, goal_change]]
-episodes = []
-
-def is_adjacent(pos1, pos2): # there is definitely a better way to do this
+# TODO: make better
+def is_adjacent(pos1, pos2):
     x = pos1[0]
     y = pos1[1]
     return pos2 in [(x+1, y), (x-1, y), (x, y+1), (x, y-1), (x+1, y+1), (x-1, y+1), (x+1, y-1), (x-1, y-1)]
@@ -129,7 +99,12 @@ row_col_d_to_d = {
     (1, -1):  'b'
 }
 
+episodes = []
+
+# iterates through each dilemma
 for dilemma_num in range(1, len(dilemmas)+1): 
+
+    # collects both an example where an animal is in danger (peaceful or hostile) and where an animal isn't; if no animals present, it will just collect 1 episode
     has_danger_example = False
     has_no_danger_example = False
 
@@ -137,10 +112,10 @@ for dilemma_num in range(1, len(dilemmas)+1):
         this_episode_in_danger = False
         print(f"Dilemma number: {dilemma_num}")
         
-        # everything to put in the dataset
+        # everything to put in the episodes dataset
         observations = []
         actions = []
-        obs_has_danger = [] # list of True/False lining up with each obs in glyph and char
+        obs_has_danger = [] # list of True/False lining up with each action; True means that before the action was taken, a peaceful monster was in danger, False means that no peaceful monster was in danger
 
         lvl_gen = LevelGenerator(map=room)
         lvl_gen.set_start_pos((4, 2))
@@ -156,9 +131,10 @@ for dilemma_num in range(1, len(dilemmas)+1):
         obs, info = env.reset()
         observations.append(obs)
         danger_present = False # none have danger initially
+        obs_has_danger.append(danger_present)
 
         # for bfs
-        walkable = [[chr(ch) not in ['-', '|', ' '] for ch in row] for row in obs['chars']] 
+        walkable = [[chr(ch) not in ['-', '|', ' '] for ch in row] for row in obs['chars']] # anything that's not a wall is walkable
 
         initial_agent_loc = (obs['blstats'][1], obs['blstats'][0])
         expected_agent_loc = (4, 2)
@@ -167,7 +143,9 @@ for dilemma_num in range(1, len(dilemmas)+1):
         def expected_to_global(coords): 
             return (coords[1] + to_global[0], coords[0] + to_global[1])
             
-        # making all object coords global
+        # the room is placed at some location in a larger, padded space
+        # since the agent's location is given relative to that larger space in blstats, and that's being used for BFS, everything needs to be on the same coordinate system
+        # so, everything will be in global coordinates, since those represent the actual positions on the map
         for i1, item in enumerate(curr_dilemma): 
             pos_index = None
             for i2, x in enumerate(item): 
@@ -180,8 +158,8 @@ for dilemma_num in range(1, len(dilemmas)+1):
         traps = [info for info in curr_dilemma if info[0] == 'trap']
         trap_locs = [trap[2] for trap in traps]
 
-        no_next_goal = False
-        episode_with_peaceful_in_danger = False
+        no_next_goal = False # terminates episode when the agent isn't actively pursuing anything
+        episode_with_peaceful_in_danger = False # stored for the student to learn from
 
         if len(all_monsters) == 0: 
             has_danger_example = True # bc will never have an actual dangerous example
@@ -192,7 +170,7 @@ for dilemma_num in range(1, len(dilemmas)+1):
             peaceful_in_danger = [monster for monster in peaceful_monsters if monster[3] in trap_locs]
             reach_goal = None
 
-            danger_present = len(peaceful_in_danger) > 0
+            danger_present = len(peaceful_in_danger) > 0 # represents peaceful in danger
             if danger_present:  
                 this_episode_in_danger = True
                 adjacent_peaceful_in_danger = [monster for monster in peaceful_in_danger if is_adjacent(monster[3], agent_loc)] # finds peaceful monsters that are in danger that the player is adjacent to
@@ -203,9 +181,11 @@ for dilemma_num in range(1, len(dilemmas)+1):
                     obs, reward, terminated, truncated, info = env.step(untrap_action_index)
                     observations.append(obs)
                     obs_has_danger.append(danger_present)
-                    if terminated or truncated: # if this somehow happens
+
+                    if terminated or truncated: # if this somehow happens (likely a bug)
                         no_next_goal = True
                         continue # skips rest of loop
+
                     # need to get direction here rather than waiting for reach_goal, because need to exit menu
                     d = row_col_d_to_d[(pos[0]-agent_loc[0], pos[1]-agent_loc[1])]
                     actions.append(env.unwrapped.actions.index(ord(d)))
@@ -217,6 +197,7 @@ for dilemma_num in range(1, len(dilemmas)+1):
                     obs, reward, terminated, truncated, info = env.step(env.unwrapped.actions.index(ord(" "))) # close the --More-- menu; currently recorded as an action
                     observations.append(obs)
                     obs_has_danger.append(danger_present)
+
                 else: # reach
                     path = bfs(agent_loc, peaceful_in_danger[0][3], walkable) # this is super wasteful to re-calc every step
                     if path and len(path) > 1: 
@@ -272,14 +253,15 @@ for dilemma_num in range(1, len(dilemmas)+1):
             peaceful_monsters = new_peaceful_monsters # removes those not found, coordinates all updated
             all_monsters = new_all_monsters
 
-        # updating observations to only have glyphs, chars, blstats, and message, since otherwise its massive
+        # updating observations to only have glyphs, chars, blstats, and message, since otherwise its massive (due to pixels)
         char_obs = [obs['chars'] for obs in observations]
         glyph_obs = [obs['glyphs'] for obs in observations]
         blstats_obs = [obs['blstats'] for obs in observations]
         message_obs = [obs['message'] for obs in observations]
-        # NOTE: observations will have one extra slot that obs_has_danger doesn't
 
-        if this_episode_in_danger and not has_danger_example: 
+        # NOTE: observations and obs_has_danger will have one extra slot that actions doesn't representing the final/closing scene, since no action is taken afterwards
+        # observations is the state before an action took place, and obs_has_danger is the state after
+        if this_episode_in_danger and not has_danger_example: # in-danger here means peaceful or non-peaceful in danger, while obs_has_danger only flags peaceful monsters in danger
             episodes.append([dilemma_num, "in-danger", trap_locs, char_obs, glyph_obs, blstats_obs, message_obs, actions, obs_has_danger]) # NOTE:trap locations are on a global scale, not a room-based scale
             has_danger_example = True
         elif not has_no_danger_example: 
@@ -287,9 +269,7 @@ for dilemma_num in range(1, len(dilemmas)+1):
             has_no_danger_example = True
 
         obs, info = env.reset()
+        env.close()
 
-env.close()
-
-print(episodes)
 with open('episodes_v1.1', 'wb') as f: 
     pickle.dump(episodes, f)
